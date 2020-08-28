a452fab328b2649512a82eef42affc418630bb69
Alan Tovar <43427055+atovav@users.noreply.github.com>
2020-08-28 17:11:23 -0500

    Fixed path

--- app/server.py
+++ app/server.py
@@ -39,7 +39,7 @@ async def download_file(url, dest):
 async def setup_learner():
 #    await download_file(export_file_url, path / export_file_name)
     try:
-        learn = load_learner('.model.pkl')
+        learn = load_learner('/app/model.pkl')
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
